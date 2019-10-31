2803368e77cfaaddba7166a31895da5184c3bf8d
shayannasir <sshayann7@gmail.com>
2019-11-01 01:18:42 +0530

    Login and register

--- app/src/main/java/com/shayannasir/projectm/LoginActivity.java
+++ app/src/main/java/com/shayannasir/projectm/LoginActivity.java
@@ -1,14 +1,23 @@
 package com.shayannasir.projectm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
@@ -16,14 +25,22 @@ import org.w3c.dom.Text;
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog loadingBar;
+
     private Button LoginButton, PhoneLoginButton;
     private EditText UserEmail, UserPassword;
     private TextView ForgotPasswordLink, NeedNewAccountLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
         InitializeFields();
 
@@ -33,6 +50,47 @@ public class LoginActivity extends AppCompatActivity {
                 SendUserToRegisterActivity();
             }
         });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowUserToLogin();
+            }
+        });
+    }
+
+    private void AllowUserToLogin() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+            Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show();
+        if(TextUtils.isEmpty(password))
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show();
+        else{
+            loadingBar.setTitle("Logging in");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                SendUserToMainActivity();
+                                Toast.makeText(LoginActivity.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else{
+                                String message = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+
     }
 
     private void InitializeFields() {
@@ -42,6 +100,7 @@ public class LoginActivity extends AppCompatActivity {
         UserPassword = (EditText) findViewById(R.id.login_password);
         ForgotPasswordLink = (TextView) findViewById(R.id.forget_password_link);
         NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        loadingBar = new ProgressDialog(this);
     }
 
     @Override
--- app/src/main/java/com/shayannasir/projectm/MainActivity.java
+++ app/src/main/java/com/shayannasir/projectm/MainActivity.java
@@ -1,14 +1,18 @@
 package com.shayannasir.projectm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TableLayout;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 //import android.support.v7.widget.Toolbar; //choose this if toolbar causes issues
 
@@ -21,11 +25,15 @@ public class MainActivity extends AppCompatActivity {
     private TabsAccessorAdapter myTabsAccessorAdapter;
 
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -52,5 +60,22 @@ public class MainActivity extends AppCompatActivity {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.main_logout_option){
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        return true;
     }
 }
