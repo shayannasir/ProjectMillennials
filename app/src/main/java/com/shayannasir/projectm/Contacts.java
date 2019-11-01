7cd143af491d5aced016f9e639c14fe1a62b315e
shayannasir <sshayann7@gmail.com>
2019-11-02 02:44:45 +0530

    Retrieve all users from database and display in Find People

---
+++ app/src/main/java/com/shayannasir/projectm/Contacts.java
@@ -0,0 +1,22 @@
+package com.shayannasir.projectm;
+
+public class Contacts {
+
+    public String name, status;
+
+    public Contacts(){
+
+    }
+
+
+    public Contacts(String name, String status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {return name;}
+    public String getStatus() {return status;}
+
+    public void setName(String name) {this.name = name;}
+    public void setStatus(String status) {this.status = status;}
+}
--- app/src/main/java/com/shayannasir/projectm/FindFriendsActivity.java
+++ app/src/main/java/com/shayannasir/projectm/FindFriendsActivity.java
@@ -1,5 +1,6 @@
 package com.shayannasir.projectm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,18 +9,73 @@ import androidx.appcompat.widget.Toolbar;
 
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FindFriendsActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private RecyclerView FindFriendsRecyclerList;
 
+    private DatabaseReference UsersRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
 
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
         InitializeFields();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options =
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(UsersRef, Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts, FindFriendsViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Contacts, FindFriendsViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindFriendsViewHolder holder, int position, @NonNull Contacts model) {
+                        holder.userName.setText(model.getName());
+                        holder.userStatus.setText(model.getStatus());
+                    }
+
+                    @NonNull
+                    @Override
+                    public FindFriendsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_display_layout, parent, false);
+                        FindFriendsViewHolder viewHolder = new FindFriendsViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+        FindFriendsRecyclerList.setAdapter(adapter);
+
+        adapter.startListening();
+    }
+
+    public static class FindFriendsViewHolder extends RecyclerView.ViewHolder{
+
+        TextView userName, userStatus;
+
+        public FindFriendsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.user_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+        }
     }
 
     private void InitializeFields() {
