d3eaee8fa0c849d48dcf0779638d7cf15429b1e4
shayannasir <sshayann7@gmail.com>
2019-11-02 21:39:21 +0530

    Created chat requests page. Populated it. Error on sender side though. Added a Toolbar

--- app/src/main/java/com/shayannasir/projectm/RequestsActivity.java
+++ app/src/main/java/com/shayannasir/projectm/RequestsActivity.java
@@ -1,16 +1,37 @@
 package com.shayannasir.projectm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RequestsActivity extends AppCompatActivity {
 
-//    private View RequestsActivityView;
+    private Toolbar mToolbar;
     private RecyclerView myRequestsList;
+
+    private DatabaseReference ChatReqRef, UsersRef;
+    private FirebaseAuth mAuth;
+
+    private String currentUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +41,108 @@ public class RequestsActivity extends AppCompatActivity {
         InitializeFields();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(ChatReqRef.child(currentUserID), Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts, RequestsViewHolder> adapter = new FirebaseRecyclerAdapter<Contacts, RequestsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final RequestsViewHolder requestsViewHolder, int i, @NonNull Contacts contacts) {
+
+                requestsViewHolder.itemView.findViewById(R.id.request_accept_button).setVisibility(View.VISIBLE);
+                requestsViewHolder.itemView.findViewById(R.id.request_reject_button).setVisibility(View.VISIBLE);
+
+                final String list_user_id = getRef(i).getKey();
+
+                DatabaseReference getTypeRef = getRef(i).child("request_type").getRef();
+
+                getTypeRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()){
+                            String type = dataSnapshot.getValue().toString();
+
+                            if(type.equals("received")){
+                                UsersRef.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if(dataSnapshot.exists()){
+                                            final String requestUserName = dataSnapshot.child("name").getValue().toString();
+                                            final String requestUserStatus = dataSnapshot.child("status").getValue().toString();
+
+                                            requestsViewHolder.userName.setText(requestUserName);
+                                            requestsViewHolder.userStatus.setText(requestUserStatus);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public RequestsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_display_layout, parent, false);
+                RequestsViewHolder holder = new RequestsViewHolder(view);
+                return holder;
+
+            }
+        };
+
+        myRequestsList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class RequestsViewHolder extends RecyclerView.ViewHolder{
+
+        TextView userName, userStatus;
+        Button AcceptButton, RejectButton;
+
+        public RequestsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.user_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+
+            AcceptButton = itemView.findViewById(R.id.request_accept_button);
+            RejectButton = itemView.findViewById(R.id.request_reject_button);
+
+        }
+    }
+
     private void InitializeFields() {
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        ChatReqRef = FirebaseDatabase.getInstance().getReference().child("ChatRequests");
 
         myRequestsList = (RecyclerView)findViewById(R.id.chat_requests_list);
         myRequestsList.setLayoutManager(new LinearLayoutManager(this));
+
+        mToolbar = (Toolbar) findViewById(R.id.chat_requests_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Chat Requests");
 
     }
 }
