424a21c9e699588b04309501dda11b340d0e4419
shayannasir <sshayann7@gmail.com>
2019-11-03 15:59:24 +0530

    Smooth scrolling and bug fixes

--- app/src/main/java/com/shayannasir/projectm/ChatActivity.java
+++ app/src/main/java/com/shayannasir/projectm/ChatActivity.java
@@ -84,6 +84,8 @@ public class ChatActivity extends AppCompatActivity {
                         messagesList.add(messages);
 
                         messageAdapter.notifyDataSetChanged();
+
+                        userMessagesList.smoothScrollToPosition(userMessagesList.getAdapter().getItemCount());
                     }
 
                     @Override
